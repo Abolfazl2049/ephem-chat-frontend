699515a1d8d377b4382fc3dc17332f1e0823139a
Enlil <116581871+Abolfazl2049@users.noreply.github.com>
2025-12-26 16:49:19 +0330

    multiple root layout

--- src/features/auth/components/client-gate.tsx
+++ src/features/auth/components/client-gate.tsx
@@ -3,45 +3,37 @@
 import { useEffect, useState } from "react";
 import { clearUserToken, getUserToken } from "../service/utils";
 import { useRouter } from "next/navigation";
-import { usePathname } from "next/navigation";
-import { getRouteConfig } from "@/configs/routes";
 import { fetchUserProfile } from "../service/fetch.util";
 
 export default function ClientGate({ children }: { children: React.ReactNode }) {
   const [ready, setReady] = useState(false);
   const router = useRouter();
-  const pathname = usePathname();
-  const routeConfig = getRouteConfig(pathname);
 
   useEffect(() => {
     async function init() {
       try {
         let profile: null | Record<string, null> = null;
 
-        if (routeConfig.requiresAuth) {
-          if (getUserToken()) {
-            profile = await fetchUserProfile();
-            console.log("profile", profile);
-          } else {
-            router.replace("/auth/create-session");
-          }
+        if (getUserToken()) {
+          profile = await fetchUserProfile();
+          console.log("profile", profile);
+        } else {
+          router.replace("/auth/create-session");
         }
 
         setReady(true);
       } catch (err) {
-        if (routeConfig.requiresAuth) {
-          clearUserToken();
-          router.replace("/auth/create-session");
-        }
+        clearUserToken();
+        router.replace("/auth/create-session");
       }
     }
 
     init();
-  }, [routeConfig.requiresAuth, router]);
+  }, [router]);
 
   if (!ready) {
     return null;
   }
 
-  return <>{children}</>;
+  return <div>sss{children}</div>;
 }
--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,2 +1,3 @@
 export { default as CreateSessionScreen } from "./screens/create-session";
 export { default as RestoreTokenScreen } from "./screens/restore-token";
+export { default as ClientGate } from "./components/client-gate";
